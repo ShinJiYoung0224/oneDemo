edc7cd4be827f4fe1767c2ba0f51db8136a4a205
jiyoung <eldud15@naver.com>
2022-02-18 13:53:44 +0900

    delete문 수정함 user, img 참조

--- sjydemo/src/main/resources/schema.sql
+++ sjydemo/src/main/resources/schema.sql
@@ -1,10 +1,13 @@
-DROP TABLE IF EXISTS user;
+
+#강제로 참조관계를 끊고 부모테이블 삭제
+DROP TABLE IF EXISTS user CASCADE CONSTRAINTS;
 
 CREATE TABLE user(
 user_no INT NOT NULL AUTO_INCREMENT,
 email VARCHAR(30),
 name VARCHAR(30),
 tel VARCHAR(30),
+CONSTRAINT user_pk
 PRIMARY KEY (user_no)
 );
 
@@ -15,5 +18,8 @@ img_no INT NOT NULL AUTO_INCREMENT,
 user_no INT NOT NULL,
 name VARCHAR(30),
 img_name VARCHAR(300),
-PRIMARY KEY (img_no)
+CONSTRAINT user_img_pk
+FOREIGN KEY (user_no)
+REFERENCES user(user_no)
+ON DELETE CASCADE
 );
